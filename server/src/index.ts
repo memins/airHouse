26b86ec4c9a0b1d57473f6a2f81688e83f04095c
Emin Sahin <m.emins@yahoo.com>
2021-10-24 07:46:37 +0200

    start the server, setup eslint && added gitignore

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,27 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { listings } from "./listings";
 
 const app = express();
 const port = 9000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(bodyParser.json());
+
+app.get("/listings", (req, res) => {
+  return res.send(listings);
+});
+
+app.post("/delete-listing", (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+
+  return res.send("failed to delete listing");
+});
 
 app.listen(port);
 
